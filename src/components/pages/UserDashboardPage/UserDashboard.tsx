6711da4f86f1a2f8414fe45f17ef568ca59ecc16
Boris <72118807+Curis-lab@users.noreply.github.com>
2025-03-15 12:50:42 +0630

    🔨 connect with calendar filtering

--- src/components/pages/UserDashboardPage/UserDashboard.tsx
+++ src/components/pages/UserDashboardPage/UserDashboard.tsx
@@ -1,6 +1,11 @@
 import EventList from "../../templates/EventList";
 import { DataFetcher } from "../../../lib/axios";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { styled } from "@mui/material";
+import { Calendar } from "react-date-range";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import React, { useState } from "react";
 
 /**
  *
@@ -12,17 +17,37 @@ import { useQuery } from "@tanstack/react-query";
  *
  */
 
+const StyledContainerWrapper = styled("div")({
+  display: "flex",
+});
+
 const UserDashboardPage = () => {
   const dataFetcher = new DataFetcher();
-
+  const queryClient = useQueryClient();
+  const [date, setDate] = useState<Date>(new Date());
   const {
     data,
     isLoading,
     isError,
     error: errorMessage,
   } = useQuery({
-    queryKey: ["events"],
-    queryFn: () => dataFetcher.fetchData("/events"),
+    queryKey: ["events", date],
+    queryFn: (date) => {
+      const currentDate = new Date();
+      const selectedDate = new Date(date.queryKey[1] as string);
+
+      if (selectedDate.toDateString() === currentDate.toDateString()) {
+        return dataFetcher.fetchData("/events");
+      } else {
+        const selectedDate = new Date(date.queryKey[1] as string)
+          .toISOString()
+          .split("T")[0];
+
+        return dataFetcher.fetchData(
+          `/events/search/by-date?date=${selectedDate}`
+        );
+      }
+    },
   });
 
   if (isLoading) {
@@ -32,10 +57,19 @@ const UserDashboardPage = () => {
   if (isError) {
     return <div>Error on something: {errorMessage}</div>;
   }
+
+  const handleDateSelection = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDate(e);
+    queryClient.invalidateQueries({ queryKey: ["events"] });
+  };
+
   return (
-    <div>
+    <StyledContainerWrapper>
+      <div>
+        <Calendar date={date} onChange={(e) => handleDateSelection(e)} />
+      </div>
       <EventList {...data} />
-    </div>
+    </StyledContainerWrapper>
   );
 };
 
